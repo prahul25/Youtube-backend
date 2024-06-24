bcd76456438772f4ad341ec5afff6117b78a103e
prahul25 <palrahulkumar2002@gmail.com>
2024-06-24 11:59:03 +0530

    Add get video comments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { Comment } from "../models/comment.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -33,12 +34,44 @@ const addCommentToVideo = asyncHandler(async (req, res) => {
 
 const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  // Your logic to get comments goes here
+
+  const comment = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+
+    {
+      $unwind: "$owner",
+    },
+  ]);
 
   return res
     .status(200)
     .json(
-      new ApiResponse(200, "Comments retrieved successfully for the video")
+      new ApiResponse(
+        200,
+        comment,
+        "Comments retrieved successfully for the video"
+      )
     );
 });
 
@@ -76,14 +109,16 @@ const updateVideoComment = asyncHandler(async (req, res) => {
 const deleteVideoComment = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
 
-  const comment = await Comment.findByIdAndDelete(commentId ,{new:true})
-  
+  const comment = await Comment.findByIdAndDelete(commentId, { new: true });
+
   if (!comment) {
     throw new ApiError(404, "Comment not found, unable to delete");
   }
   return res
     .status(200)
-    .json(new ApiResponse(200, {},"Comment deleted successfully from the video"));
+    .json(
+      new ApiResponse(200, {}, "Comment deleted successfully from the video")
+    );
 });
 
 export {
